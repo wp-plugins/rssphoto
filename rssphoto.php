b44ad0a18c55dc250bb5993c4dd3bf0d6855a1d7
spencerkellis <spencerkellis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-11 02:50:19 +0000

    Added GPL license info
    git-svn-id: https://plugins.svn.wordpress.org/rssphoto/trunk@144640 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rssphoto.php
+++ rssphoto.php
@@ -1,12 +1,31 @@
 <?php
 /*
  * Plugin Name: RSSPhoto
- * Version: 0.2
  * Plugin URI: http://blog.spencerkellis.net/projects/rssphoto
  * Description: Display photos from an RSS or Atom feed
+ * Version: 0.2
  * Author: Spencer Kellis
  * Author URI: http://blog.spencerkellis.net
+ *
  */
+
+/*  Copyright 2009 Spencer Kellis (email : spencerkellis *AT* gmail)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
 class RSSPhotoWidget extends WP_Widget
 {
