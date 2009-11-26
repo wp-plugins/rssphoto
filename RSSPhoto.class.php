f4395de3f3503fc8fcb44f4e060f0fc319a5900a
spencerkellis <spencerkellis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-26 18:46:44 +0000

    committed v0.6.8: beta capability for displaying item titles with thumbnails
    git-svn-id: https://plugins.svn.wordpress.org/rssphoto/tags/0.6.8@177294 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ RSSPhoto.class.php
@@ -0,0 +1,485 @@
+<?php
+
+/*  Copyright 2009 Spencer Kellis (email : spencerkellis *AT* gmail)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class RSSPhoto
+{
+  /****************************
+   * Internal variables
+   ****************************/
+  var $images         = array();
+  var $error_msg      = "";
+  var $id             = -1;
+  var $show_desc      = false;
+  var $mime_types     = array('image/jpeg','image/jpg','images/jpeg','image/gif','image/png');
+  var $medium_types   = array('image');
+
+  /****************************
+   * RSSPhoto temp vars
+   ****************************/
+  var $rss_type_src   = 'Choose'; // 'Choose' or 'Enclosures' or 'Description' or 'Content'*
+
+  /****************************
+   * RSSPhoto settings
+   ****************************/
+  var $title          = 'RSSPhoto';
+  var $url            = 'http://photography.spencerkellis.net/atom.php';
+  var $fixed          = 'Max';
+  var $size           = 150;
+  var $img_sel        = 'Most Recent';
+  var $num_img        = 1;
+  var $min_size       = 10;
+  var $item_sel       = 'Random';
+  var $num_item       = 1;
+  var $show_title     = 0;
+  var $output         = 'Slideshow';
+  var $interval       = 6000;
+
+  /****************************
+   * SimplePie settings
+   ****************************/
+  var $feed;
+  var $cache_location = 'wp-content/cache';
+  var $force_feed     = false;
+
+  function RSSPhoto($settings=array())
+  {
+    $this->update($settings);
+  }
+
+  function init()
+  {
+    $this->id = rand();
+
+    // initialize SimplePie object
+    $this->feed = new SimplePie();
+    $this->feed->set_cache_location($this->cache_location);
+    $this->feed->set_feed_url($this->url);
+    $this->feed->force_feed($this->force_feed);
+    $this->feed->init();
+
+    // get images, generate thumbnails, etc.
+    if($this->feed->get_item_quantity() > 0)
+    {
+      $item_idxs = $this->select_indices($this->feed->get_items(),$this->item_sel,$this->num_item);
+
+      // choose feed item(s)
+      foreach($item_idxs as $item_idx)
+      {
+        $item = $this->feed->get_item($item_idx);
+
+        if($item != false)
+        {
+          // pull out image url, link
+          if($this->feed->get_type() & SIMPLEPIE_TYPE_RSS_ALL)
+          {
+            switch($this->rss_type_src)
+            {
+              case 'Enclosures':
+                $image_url = $this->get_img_urls($enclosures,$this->img_sel,$this->num_img,'Enclosures');
+                break;
+              case 'Description':
+                $image_url = $this->get_img_urls($item,$this->img_sel,$this->num_img,'Description');
+                break;
+              case 'Content':
+                $image_url = $this->get_img_urls($item,$this->img_sel,$this->num_img,'Content');
+                break;
+              case 'Choose':
+                if($enclosures = $item->get_enclosures())
+                  $image_url = $this->get_img_urls($enclosures,$this->img_sel,$this->num_img,'Enclosures');
+                else
+                  $image_url = $this->get_img_urls($item,$this->img_sel,$this->num_img,'Description');
+                break;
+            }
+          }
+          elseif ($this->feed->get_type() & SIMPLEPIE_TYPE_ATOM_ALL)
+          {
+            $image_url = $this->get_img_urls($item,$this->img_sel,$this->num_img,'Content');
+          }
+
+          if(is_array($image_url))
+          {
+            foreach($image_url as $url)
+            {
+              $thumb_url = $this->create_thumbnail($url,$fixed,$size,$min_size);
+              if($thumb_url!=false)
+                $this->add_image($thumb_url,$item->get_link(0),$item->get_description(),$item->get_title());
+            }
+          }
+          else
+          {
+            $this->set_error('$image_url isn\'t an array');
+          }
+        }
+        else // item==false
+        {
+          $this->error_msg="<p>Tried to load item #$item_idx from <a href=\"$url\">$url</a> and couldn't!</p>";
+          if($this->feed->error())
+          {
+            $this->error_msg.="<p>The SimplePie error was ";
+            $this->error_msg.=$this->feed->error();
+            $this->error_msg.="</p>";
+          }
+          $this->title=apply_filters('widget_title', 'Oops!');
+        }
+      }
+    }
+  }
+
+  function html()
+  {
+    switch($this->output)
+    {
+      case 'Slideshow':
+        return $this->slideshow_html();
+        break;
+      case 'Static':
+      default:
+        return $this->static_html();
+        break;
+    }
+  }
+
+  /**
+  * Create thumbnails of a given image url in the local cache
+  *
+  */
+  function create_thumbnail($image_url)
+  {
+    $image_filename = "rssphoto-".md5($image_url)."-{$this->fixed}-{$this->size}.jpg";
+    $thumb_path = $this->cache_location."/".$image_filename;
+    $thumb_url = get_bloginfo('wpurl')."/$thumb_path";
+    if(!file_exists($thumb_path))
+    {
+      // attempt to get image dimensions using getimagesize
+      list($width, $height, $type, $attr) = @getimagesize($image_url);
+      
+      // if that doesn't work, check for GD and use imagesx/imagesy
+      if($height==false || $width==false)
+      {
+        if($type==1 && function_exists('imagecreatefromgif'))
+          $image = @imagecreatefromgif($image_url);
+        if($type==2 && function_exists('imagecreatefromjpeg'))
+          $image = @imagecreatefromjpeg($image_url);
+        elseif($type==3 && function_exists('imagecreatefrompng'))
+          $image = @imagecreatefrompng($image_url);
+
+        if($image==false)
+          return false;
+        else
+        {
+          $height = @imagesy($image);
+          $width = @imagesx($image);
+        }
+      }
+     
+      if($height<=$this->min_size || $width<=$this->min_size)
+        return false;
+      
+      // if we've got valid image dimensions, continue
+      if($height!=false && $width!=false)
+      {
+        // default parameters
+        switch($this->fixed)
+        {
+          case 'Width':
+            $ratio = $this->size/$width;
+            $thumb_width = $this->size;
+            $thumb_height = $height * $ratio;
+            break;
+          case 'Height':
+            $ratio = $this->size/$height;
+            $thumb_height = $this->size;
+            $thumb_width = $width * $ratio;
+            break;
+          case 'Max':
+          default:
+            $ratio = $this->size/max($height,$width);
+            if($width>$height)
+            {
+              $thumb_width = $this->size;
+              $thumb_height = $height * $ratio;
+            }
+            else
+            {
+              $thumb_height = $this->size;
+              $thumb_width = $width * $ratio;
+            }
+            break;
+        }
+     
+        // use GD library to create cached thumbnail if necessary
+        if(function_exists('imagecreatefromjpeg'))
+        {
+          // create thumbnail
+          if($image == false)
+          {
+            if($type==1 && function_exists('imagecreatefromgif'))
+              $image = @imagecreatefromgif($image_url);
+            if($type==2 && function_exists('imagecreatefromjpeg'))
+              $image = @imagecreatefromjpeg($image_url);
+            elseif($type==3 && function_exists('imagecreatefrompng'))
+              $image = @imagecreatefrompng($image_url);
+          }
+     
+          if($image != false)
+          {
+            $quality = 85;
+            $thumb=imagecreatetruecolor($thumb_width,$thumb_height);
+            @imagecopyresampled($thumb,$image,0,0,0,0,$thumb_width,$thumb_height,$width,$height);
+            @imagejpeg($thumb,$thumb_path,$quality);
+          }
+        }
+      } // if($height!=false && width!=false)
+    } // if(!file_exists($thumb_path))
+
+    return $thumb_url;
+
+  } // function create_thumbnail
+
+  /**
+  * Get indices of images from array
+  *
+  */
+  function select_indices($arr,$method,$num)
+  {
+    $num = ($num < count($arr)) ? $num : count($arr);
+    switch($method)
+    {
+      case 'Most Recent':
+        $idxs = range(0,$num-1);
+        break;
+      case 'Random':
+      default:
+        if($num==1)
+          $idxs[0] = rand(0,count($arr)-1);
+        else
+          $idxs = array_rand($arr,$num);
+        break;
+    }
+    return $idxs;
+  }
+
+  /**
+  * Check array elements
+  *
+  */
+  function check_indices($arr)
+  {
+    for($k=count($arr)-1; $k>=0; $k--)
+    {
+      // check for empty object
+      if(empty($arr[$k]))
+      {
+        unset($arr[$k]);
+        break;
+      }
+
+      // if it's an enclosure... anything else?
+      if(is_object($arr[$k]))
+      {
+        // check for url 
+        if(method_exists($arr[$k],'get_link'))
+        {
+          $lnk=$arr[$k]->get_link(); // some bug that kills php if this isn't separated out
+          if(empty($lnk))
+          {
+            unset($arr[$k]); 
+            break;
+          }
+        }
+       
+        // check for compatible image type
+        if(method_exists($arr[$k],'get_type') || method_exists($arr[$k],'get_medium'))
+        {
+          $mime_flag=0;
+          $medium_flag=0;
+          if(method_exists($arr[$k],'get_type'))
+          {
+            foreach($this->mime_types as $mime)
+            {
+              if(!strcmp($arr[$k]->get_type(),$mime))
+              {
+                $mime_flag=1;
+                break;
+              }
+            }
+          }
+          if(method_exists($arr[$k],'get_medium'))
+          {
+            foreach($this->medium_types as $medium)
+            {
+              if(!strcmp($arr[$k]->get_medium(),$medium))
+              {
+                $medium_flag=1;
+                break;
+              }
+            }
+          }
+          // if neither mime or medium, discard
+          if(!($mime_flag | $medium_flag))
+          {
+            unset($arr[$k]);
+            break;
+          }
+        }
+      }
+    }
+
+    $arr=array_values($arr);
+    return $arr;
+  }
+
+  /**
+  * Add image to array
+  *
+  */
+  function add_image($url=false,$link="",$desc="",$title="")
+  {
+    if($url!=false)
+    {
+      $idx=count($this->images);
+      $this->images[$idx]['url']=$url;
+      $this->images[$idx]['link']=$link;
+      $this->images[$idx]['desc']=$desc;
+      $this->images[$idx]['title']=$title;
+    }
+  }
+
+  function slideshow_html()
+  {
+    $html = '<div class="rssphoto_slideshow" id="rssphoto-'.$this->id.'" style="height:'.$this->size.'px;">';
+
+    $active=0;
+    foreach($this->images as $img)
+    {
+      $html .= '<div class="item ';
+      if(!$active)
+      {
+        $html .= 'active'; 
+        $active=1;
+      }
+      $html .= '">';
+      if($this->show_title && !empty($img['title']))
+      {
+        $html .= '<div class="rssphoto_item_title">'.$img['title'].'</div>';
+      }
+      $html .= '<a href="'.$img['link'].'"><img src="'.$img['url'].'" alt="" /></a>';
+      $html .= '</div>';
+      $html .= "\n";
+    }
+    $html .= '</div>';
+    $html .= "\n";
+
+    if(count($this->images)>1)
+    {
+      $html .= '<script type="text/javascript">setInterval( "slideSwitch('.$this->id.')", '.$this->interval.' );</script>';
+      $html .= "\n";
+    }
+
+    return $html;
+  }
+
+  function static_html()
+  {
+    $html = '<div class="rssphoto_static" id="rssphoto-'.$this->id.'">';
+    foreach($this->images as $img)
+    {
+      $html .= '<div>';
+      if($this->show_title && !empty($img['title']))
+      {
+        $html .= '<div class="rssphoto_item_title">'.$img['title'].'</div>';
+      }
+      $html .= '<a href="'.$img['link'].'"><img src="'.$img['url'].'"></a></div>';
+      if($this->show_desc)
+      {
+        $html .= '<p>'.$img['desc'].'</p>';
+      }
+      $html .= "\n";
+    }
+    $html .= '</div>';
+    $html .= "\n";
+
+    $html .= '<script type="text/javascript">expandStatic('.$this->id.');</script>';
+    $html .= "\n";
+
+    return $html;
+  }
+
+  function title()
+  {
+    return $this->title;
+  }
+
+  function update($settings)
+  {
+    if(!empty($settings['rssphoto_title']))      $this->title      = $settings['rssphoto_title'];
+    if(!empty($settings['rssphoto_url']))        $this->url        = $settings['rssphoto_url'];
+    if(!empty($settings['rssphoto_fixed']))      $this->fixed      = $settings['rssphoto_fixed'];
+    if(!empty($settings['rssphoto_size']))       $this->size       = $settings['rssphoto_size'];
+    if(!empty($settings['rssphoto_img_sel']))    $this->img_sel    = $settings['rssphoto_img_sel'];
+    if(!empty($settings['rssphoto_num_img']))    $this->num_img    = $settings['rssphoto_num_img'];
+    if(!empty($settings['rssphoto_item_sel']))   $this->item_sel   = $settings['rssphoto_item_sel'];
+    if(!empty($settings['rssphoto_num_item']))   $this->num_item   = $settings['rssphoto_num_item'];
+    if(!empty($settings['rssphoto_show_title'])) $this->show_title = $settings['rssphoto_show_title'];
+    if(!empty($settings['rssphoto_output']))     $this->output     = $settings['rssphoto_output'];
+    if(!empty($settings['rssphoto_interval']))   $this->interval   = $settings['rssphoto_interval'];
+  }
+
+  function get_img_urls($arr,$sel,$num,$field)
+  {
+    switch($field)
+    {
+      case 'Content':
+        preg_match_all('/img([^>]*)src="([^"]*)"/i', $arr->get_content(), $m);
+        if(count($m[2])>0)
+        {
+          $img_idxs = $this->select_indices($m[2],$sel,$num);
+          foreach($img_idxs as $img_idx)
+            $urls[count($urls)] = htmlspecialchars_decode($m[2][$img_idx]);
+        }
+        break;
+      case 'Description':
+        preg_match_all('/img([^>]*)src="([^"]*)"/i', $arr->get_description(), $m);
+        if(count($m[2])>0)
+        {
+          $img_idxs = $this->select_indices($m[2],$sel,$num);
+          foreach($img_idxs as $img_idx)
+            $urls[count($urls)] = htmlspecialchars_decode($m[2][$img_idx]);
+        }
+        break;
+      case 'Enclosures':
+        $arr=$this->check_indices($arr);
+        $img_idxs = $this->select_indices($arr,$sel,$num);
+        foreach($img_idxs as $idx)
+        {
+          if(method_exists($arr[$idx],'get_thumbnail') && $thumbnail=$arr[$idx]->get_thumbnail(0))
+            $urls[count($urls)] = htmlspecialchars_decode($thumbnail);
+          elseif(method_exists($arr[$idx],'get_link') && $lnk=$arr[$idx]->get_link())
+            $urls[count($urls)] = htmlspecialchars_decode($lnk);
+        }
+        break;
+    }
+    return $urls;
+  }
+
+  function set_error($str)
+  {
+    $this->error_msg=$str;
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,265 @@
+=== RSSPhoto ===
+Contributor: spencerkellis
+Donation Link: http://blog.spencerkellis.net/projects/rssphoto
+Tags: RSS, Atom, photoblog, photo, photography, widget, jQuery, slideshow, multi-widget, shortcode
+Requires at least: 2.8
+Tested up to: 2.8.6
+Stable tag: 0.6.8
+
+A customizable plugin to display photos from an RSS or Atom feed as a widget or shortcode.
+
+== Description ==
+
+RSSPhoto is a Wordpress plugin to display photos from RSS and Atom feeds. It includes a widget for easy addition to a sidebar, or it can be configured by shortcode to display on any page.  Easy theme integration is also possible.  RSSPhoto includes jQuery-powered, cross-browser compatible slideshow as well as static image display.
+
+RSSPhoto requires the SimplePie Core Wordpress plugin to parse RSS and Atom feeds.  A cache directory, writable by the server, is required for thumbnail storage.  The GD library is required for generating thumbnails.  If the GD library is not present, the script will default to displaying images with the img width/height attributes forced to thumbnail size.
+
+
+== Installation ==
+
+Here are the basic installation instructions:
+
+   1. Install the SimplePie Core plugin if it's not already installed ([Link](http://wordpress.org/extend/plugins/simplepie-core/ "SimplePie Core plugin")).
+   2. If it doesn't already exist, create the directory `/wp-content/cache` and give it permissions of 755
+   3. Upload all files to the `/wp-content/plugins/` directory (consider creating an rssphoto subdirectory to hold the plugin files)
+   4. Activate the plugin through the 'Plugins' menu in Wordpress
+
+Probably the easiest way to accomplish step 1 is through an FTP program.  If you're interested, here's how to do it on the command line:
+
+   1. cd {blog-dir}/wp-content
+   2. mkdir cache
+   3. chmod 755 cache
+
+To use the widget:
+
+   1. Navigate to the 'Widgets' section of the 'Appearance' menu in Wordpress
+   2. Drag the widget to the sidebar
+   3. Configure the widget as needed
+
+To use the shortcode:
+
+   1. Edit the page you want to display the images
+   2. Add the following text to the page:
+
+      [rssphoto url="your.url.com"]
+
+   3. Include any of the following attributes (see descriptions of these settings below)
+
+      * output="Slideshow|Static"
+      * url="your.url.com"
+      * item_sel="Random|Most Recent"
+      * num_item="[Number of feed items]"
+      * num_img="[Number of images per feed item]"
+      * img_sel="Random|Most Recent"
+      * fixed="Width|Height|Max"
+      * size="[size in pixels]"
+
+To integrate with a theme:
+
+   1. The plugin needs to be installed and activated.
+   2. Copy and paste the contents of the file RSSPhotoTheme.functions.php to the end of the file functions.php in your theme directory.
+   3. Declare RSSPhoto settings (multiple instances are supported).
+   4. Call the function display_rssphoto() from your theme (e.g., sidebar.php). An example of the last two steps:
+
+      `<?php
+      $settings[0]['title']='RSSPhoto';
+      $settings[0]['url']='http://photography.spencerkellis.net/rss.php';
+      $settings[0]['output']='Slideshow';
+      $settings[0]['num_item']=10;
+      $settings[0]['interval']=10000;
+      $settings[0]['fixed']='Height';
+      $settings[0]['size']=80;
+      $settings[0]['before_title']='<h2>';
+      $settings[0]['after_title']='</h2>';
+      $settings[0]['before_html']='<li>';
+      $settings[0]['after_html']='</li>';
+      display_rssphoto($settings[0]);
+      ?>`
+
+Here's a quick description of the settings:
+
+   1. Title: text that appears over the image in the sidebar.
+   2. Output: images will load as a slideshow or can be displayed statically
+   3. URL: address of the RSS or Atom feed.
+   4. Item Selection: RSSPhoto will choose a random feed item, or the most recent feed item
+   5. # Items: choose how many feed items to display
+   6. # Images per Item: how many images embedded in each feed item to display
+   7. Image selection: the script can randomly select images or just display the first image in the feed item
+   8. Fixed dimension: select whether the width, height, or longest side should be fixed.
+   9. Size (px): size in pixels of the width, height, or longest side (previously selected).
+
+
+== Frequently Asked Questions ==
+
+= How do I change the title, feed URL, or dimensions? =
+
+After your widget appears in the sidebar, go to the 'Widgets' section under the 'Appearance' menu in Wordpress and open the settings for the widget (click the down arrow in the widget titlebar and the form will appear).  Modify the fields as needed and click save.
+
+= I'm getting an error about the SimplePie class not being found.  What's wrong? =
+
+Here's what the error might look like:
+
+Fatal error: Class .SimplePie. not found in /home/username/public_html/wp-content/plugins/rssphoto/rssphoto.php on line 40
+
+If you receive this error, the most likely problem is that the SimplePie Core plugin is not installed or activated.  Here's a link to the [SimplePie Core plugin](http://wordpress.org/extend/plugins/simplepie-core/ "SimplePie Core plugin").
+
+= I can't seem to get thumbnails larger than X by Y pixels =
+
+Check to make sure the photos in your feed are larger than X by Y pixels - RSSPhoto uses the image actually embedded in the feed to generate locally cached thumbnails.
+
+= Is there a way to prevent very small images from being displayed? =
+
+Yes, you can set a variable to require a minimum size (in pixels) of either width or height.  In `RSSPhoto.class.php`, look for 
+
+  var $min_size = 10;
+
+And change the value as needed (default is 10 pixels).
+
+= My feed doesn't display any photos; W3C Feed Validation says it's valid but has a warning about wrong media type =
+
+If you get a warning from the [W3C Feed Validation Service](http://validator.w3.org "W3C Feed Validation Service") about your feed being served with the wrong media type, and RSSPhoto doesn't display your images, it may be an issue where SimplePie refuses to parse the feed because of the incorrect media type.  Open `RSSPhoto.class.php` and set the `$force_feed` variable to `true`:
+
+var $force_feed = true;
+
+= My feed doesn't display any photos and there are no problems with the feed validation =
+
+RSS feeds can be implemented in numerous ways.  RSSPhoto attempts to intelligently find the pictures in an RSS feed, but sometimes you need to point it in the right direction.  In `RSSPhoto.class.php`, try changing the value of the variable `$rss_type_src` to one of the following values: 'Choose' (default), 'Description', 'Content', or 'Enclosures'.
+
+= How do I display the title for each image? =
+
+In `RSSPhoto.class.php`, set the variable `$show_title` to 1.  Note that this capability is still in beta, and there are some layout issues that may crop up when the title is displayed.  You can modify the CSS of the title (which is wrapped in a div just before the image) in `rssphoto.css` under the class `rssphoto_item_title`.
+
+
+== Screenshots ==
+
+1.  What the plugin looks like in my [Sidebar](http://blog.spencerkellis.net "RSSPhoto loaded in my sidebar").
+2.  The plugin settings in WP Admin -> Appearances -> Widgets.
+
+
+== Changelog ==
+
+v0.6.8
+
+* Various stability improvements
+* Added the (beta-version) capability to show image titles although the option has not been extended to the widget settings UI yet (see FAQ)
+
+
+v0.6.7
+
+* Compatibility update for yet another different mime type that means "jpeg"
+
+
+v0.6.6
+
+* Stability update: works "out-of-the-box" with all the feeds I've encountered so far in troubleshooting, except where SimplePie settings need to be changed
+* Performance improvements: removed an http request from the critical path for thumbnail generation
+* When embedded images are small, RSSPhoto will now allow upscaled locally cached thumbnails (previously, images would not be displayed at all)
+* Added support for wordpress.com feeds
+
+
+v0.6.5
+
+* Improved RSS parsing code to handle more implementation varieties (specifically better handling of enclosures)
+
+
+v0.6.4
+
+* Added support for MobileMe (Apple) RSS feeds
+
+
+v0.6.3
+
+* Added support for theme integration
+
+
+v0.6.2
+
+* Added a method for retrieving images from different portions of an RSS 2.0 feed (Atom feeds remain the same)
+
+
+v0.6.1
+
+* Add support for images contained in RSS enclosure tags
+* Fixed an annoyance where slideshow would play with only one image
+
+
+v0.6
+
+* Add shortcode support allowing RSSPhoto to be displayed on any page
+* Further improved code organization, separating RSSPhoto code from shortcode and widget code.
+* Re-introduced multi-widget support
+
+
+v0.5.1
+
+* Bug fix; v0.5 required PHP5. v0.5.1 should restore PHP4 support.
+
+
+v0.5
+
+* Added jQuery slideshow option (default)
+* Intelligently selects content or description tag based on feed type
+* Streamlined options to make widget configuration simpler
+* Improved code organization with better class structure and readability
+* Separated Javascript, CSS, and PHP; separated HTML internally
+* !Important: Removed multi-widget support!
+* Added support for solution to display images from a feed with incorrectly identified media type
+
+
+v0.4
+
+* Added support for multiple images and multiple feed items
+* Added support for filtering out images smaller than a user-defined pixel value (height or width)
+* Reconfigured to the Widget options panel to reduce the height
+
+
+v0.3.3
+
+* Added support for PNGs and GIFs
+
+
+v0.3.2
+
+* Improvement on previous bug fix to use bloginfo 'wpurl' variable to form thumbnail URL
+* Separate out image selection from feed item selection (so you can select a random image from the most recent
+  feed item or vice versa)
+* Improve the display of the Widgets settings as more options were becoming available
+
+
+v0.3.1
+
+* Major bug fix with thumbnail URLs pointing to the wrong location
+
+
+v0.3
+
+* Allow multiple RSSPhoto widgets to be present simultaneously
+* Selection of fixed width, height, or max dimension for thumbnails
+* Selection of pulling images from Content tags or Description tags from feeds
+* Fixed a bug where changing the thumbnail size wouldn't be reflected due to cached thumbnails with the same filename
+
+
+v0.2
+
+* Rolled up all other bug fixes from 0.1.* plus fixed a bug where there was no default value for the Image Selection field.
+
+
+v0.1.2
+
+* Fixed a bug where the title may not have been saved properly
+
+
+v0.1.1
+
+* Fixed an issue where the Max dimensions field had the wrong label
+
+
+v0.1
+
+* Original version
+* Specify widget title
+* Accepts any RSS or Atom feed URL
+* Customize thumbnail size
+* Select random or most recent image selection method
+* jQuery image sliding effects with graceful degradation to plain Javascript
+
